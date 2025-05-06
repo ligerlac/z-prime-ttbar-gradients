994ea020244895517ea82cbf66d7534c571e6cbe
ligerlac <lino.oscar.gerlach@cern.ch>
2025-05-06 18:26:20 +0200

    jaxify random pt noise

--- utils/systematics.py
+++ utils/systematics.py
@@ -7,6 +7,8 @@ def jet_pt_resolution(pt):
     counts = ak.num(pt)
     pt_flat = ak.flatten(pt)
     resolution_variation = np.random.normal(np.ones_like(pt_flat), 0.05)
+    resolution_variation = ak.from_numpy(resolution_variation)
+    resolution_variation = ak.to_backend(resolution_variation, ak.backend(pt_flat))
     return ak.unflatten(resolution_variation, counts)
 
 def jet_pt_scale():
