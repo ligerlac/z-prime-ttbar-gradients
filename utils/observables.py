f4a579e5a6ca299f8f093e89e9d18c1fe4bd2bed
ligerlac <lino.oscar.gerlach@cern.ch>
2025-05-13 16:58:31 +0200

    calculate mva score on the fly in observable

--- utils/observables.py
+++ utils/observables.py
@@ -1,5 +1,8 @@
+import numpy as np
 import awkward as ak
 import vector
+
+from tensorflow.keras.models import load_model
 
 # -----------------------------
 # Register backends
@@ -74,3 +77,82 @@ def get_mtt_sq(muons, jets, fatjets, met):
     )
 
     return mtt**2
+
+
+def get_mva_score(
+    jets: ak.Array,
+    muons: ak.Array,
+    model_path: str = "output/model.keras",
+) -> ak.array:
+    """Get MVA score from jets and muons.
+    Args:
+        jets (ak.Array): Jets array.
+        muons (ak.Array): Muons array.
+        model_path (str): Path to the MVA model.
+    Returns:
+        np.ndarray: MVA score (flat numpy array)
+    """
+    def _get_mva_vars(jets: ak.Array, muons: ak.Array) -> dict[str, np.ndarray]:
+        """Get MVA variables from jets and muons.
+        Args:
+            jets (ak.Array): Jets array.
+            muons (ak.Array): Muons array.
+        Returns:
+            dict[str, np.ndarray]: Dictionary of MVA variables (flat numpy arrays)
+        """
+        assert ak.all(ak.num(jets, axis=1) >= 2), "Require at least 2 jets for MVA variables"
+        assert ak.all(ak.num(muons, axis=1) == 1), "Require exactly 1 muon for MVA variables"
+
+        d = {}
+
+        # number of jets
+        d["n_jet"] = ak.num(jets, axis=1).to_numpy()
+
+        # leading and subleading jet mass
+        d["leading_jet_mass"] = jets.mass[:, 0].to_numpy()
+        d["subleading_jet_mass"] = jets.mass[:, 1].to_numpy()
+
+        # scalar sum ST
+        d["st"] = (
+            ak.sum(jets.pt, axis=1) + ak.sum(muons.pt, axis=1)
+        ).to_numpy()
+
+        # leading and subleading jet b-tag score
+        d["leading_jet_btag_score"] = jets.btagDeepB[:, 0].to_numpy()
+        d["subleading_jet_btag_score"] = jets.btagDeepB[:, 1].to_numpy()
+
+        # Sphericity tensor (only zz component)
+        denominator = ak.sum(jets.px**2 + jets.py**2 + jets.pz**2, axis=1)
+        s_zz = ak.sum(jets.pz * jets.pz, axis=1) / denominator
+        d["S_zz"] = s_zz.to_numpy()
+
+        # deltaR between muon and closest jet
+        muon_in_pair, jet_in_pair = ak.unzip(ak.cartesian([muons, jets]))
+        delta_r = muon_in_pair.deltaR(jet_in_pair)
+        min_delta_r = ak.min(delta_r, axis=1)
+        d["deltaR"] = min_delta_r.to_numpy()
+
+        # transverse momentum of the muon w.r.t. the axis of the nearest jet (pt_rel)
+        min_delta_r_indices = ak.argmin(delta_r, axis=1, keepdims=True)
+        angle = muons.deltaangle(jet_in_pair[min_delta_r_indices])
+        d["pt_rel"] = (muons.p * np.sin(angle)).to_numpy().flatten()
+
+        # deltaR between muon and closest jet times the jet pt
+        min_delta_r_indices = ak.argmin(delta_r, axis=1, keepdims=True)
+        closest_jet_pt = jet_in_pair.pt[min_delta_r_indices]
+        d["deltaR_times_pt"] = (min_delta_r * closest_jet_pt).to_numpy().flatten()
+
+        return d
+
+    model = load_model(model_path)
+
+    scores = np.zeros(len(jets), dtype=np.float32) - 1
+
+    idx = ((ak.num(jets, axis=1) >= 2) & (ak.num(muons, axis=1) == 1)).to_numpy()
+
+    mva_vars = _get_mva_vars(jets[idx], muons[idx])
+    X = np.column_stack(list(mva_vars.values())).astype(float)
+
+    scores[idx] = model.predict(X, batch_size=1024).flatten()
+
+    return scores
