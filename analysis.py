98d19791274da7e0a8ab1cf93c7236c2403ec8f5
maly <mohamedaymanali@gmail.com>
2025-05-29 23:05:40 +0300

    split analysis class to base and child to prep for diff and non-diff versions

--- analysis.py
+++ analysis.py
@@ -66,11 +66,10 @@ def is_jagged(arraylike) -> bool:
     except Exception:
         return False
 
-
 # -----------------------------
-# ZprimeAnalysis Class Definition
+# Base class
 # -----------------------------
-class ZprimeAnalysis:
+class Analysis:
     def __init__(self, config: dict[str, Any]) -> None:
         """
         Initialize ZprimeAnalysis with configuration for systematics, corrections,
@@ -89,53 +88,6 @@ class ZprimeAnalysis:
         self.corrlib_evaluators = self._load_correctionlib()
         self.nD_hists_per_region = self._init_histograms()
 
-    def _init_histograms(self) -> dict[str, dict[str, hist.Hist]]:
-        """
-        Initialize histograms for each analysis channel based on configuration.
-
-        Returns
-        -------
-        dict
-            Dictionary of channel name to hist.Hist object.
-        """
-        histograms = defaultdict(dict)
-        for channel in self.channels:
-            chname = channel.name
-            if (req_channels := self.config.general.channels) is not None:
-                if chname not in req_channels:
-                    continue
-
-            for observable in channel["observables"]:
-
-                observable_label = observable["label"]
-                observable_binning = observable["binning"]
-                observable_name = observable["name"]
-
-                if isinstance(observable_binning, str):
-                    low, high, nbins = map(
-                        float, observable_binning.split(",")
-                    )
-                    axis = hist.axis.Regular(
-                        int(nbins),
-                        low,
-                        high,
-                        name="observable",
-                        label=observable_label,
-                    )
-                else:
-                    axis = hist.axis.Variable(
-                        observable_binning,
-                        name="observable",
-                        label=observable_label,
-                    )
-
-                histograms[chname][observable_name] = hist.Hist(
-                    axis,
-                    hist.axis.StrCategory([], name="process", growth=True),
-                    hist.axis.StrCategory([], name="variation", growth=True),
-                    storage=hist.storage.Weight(),
-                )
-        return histograms
 
     def _load_correctionlib(self) -> dict[str, CorrectionSet]:
         """
@@ -384,7 +336,121 @@ class ZprimeAnalysis:
                 else weights
             )
 
-    def apply_selection_and_fill(
+
+    def compute_ghost_observables(
+        self, obj_copies: dict[str, ak.Array], tracing: bool = False
+    ) -> dict[str, ak.Array]:
+        for ghost in self.config.ghost_observables:
+
+            # do not compute if this function is being traced by JAX
+            # and observable function is not compatible with JAX
+            if not ghost.works_with_jax and tracing:
+                continue
+            logger.info(f"Computing ghost observables {ghost.names}")
+            ghost_args = self._get_function_arguments(ghost["use"], obj_copies)
+            ghost_outputs = ghost["function"](*ghost_args)
+
+            if not isinstance(ghost_outputs, (list, tuple)):
+                ghost_outputs = [ghost_outputs]
+
+            names = (
+                ghost.names if isinstance(ghost.names, list) else [ghost.names]
+            )
+            colls = (
+                ghost.collections
+                if isinstance(ghost.collections, list)
+                else [ghost.collections] * len(names)
+            )
+            # update object_copies with ghost outputs
+            for out, name, coll in zip(ghost_outputs, names, colls):
+                if (
+                    isinstance(out, ak.Array)
+                    and len(ak.fields(out)) == 1
+                    and name in out.fields
+                ):
+                    out = out[name]
+                if coll in obj_copies:
+                    try:
+                        # add new field to existing awkward array
+                        obj_copies[coll][name] = out
+                    # happens if we are adding a field to an awkward
+                    # array with no fields (scalar fields)
+                    except ValueError as e:
+                        raise e
+                else:
+                    # create new awkward array with single field
+                    obj_copies[coll] = ak.Array({name: out})
+        return obj_copies
+
+# -----------------------------
+# ZprimeAnalysis Class Definition
+# -----------------------------
+class NonDiffAnalysis(Analysis):
+
+    def __init__(self, config: dict[str, Any]) -> None:
+        """
+        Initialize ZprimeAnalysis with configuration for systematics, corrections,
+        and channels.
+
+        Parameters
+        ----------
+        config : dict
+            Configuration dictionary with 'systematics', 'corrections', 'channels',
+            and 'general'.
+        """
+        super().__init__(config)
+        self.nD_hists_per_region = self._init_histograms()
+
+    def _init_histograms(self) -> dict[str, dict[str, hist.Hist]]:
+        """
+        Initialize histograms for each analysis channel based on configuration.
+
+        Returns
+        -------
+        dict
+            Dictionary of channel name to hist.Hist object.
+        """
+        histograms = defaultdict(dict)
+        for channel in self.channels:
+            chname = channel.name
+            if (req_channels := self.config.general.channels) is not None:
+                if chname not in req_channels:
+                    continue
+
+            for observable in channel["observables"]:
+
+                observable_label = observable["label"]
+                observable_binning = observable["binning"]
+                observable_name = observable["name"]
+
+                if isinstance(observable_binning, str):
+                    low, high, nbins = map(
+                        float, observable_binning.split(",")
+                    )
+                    axis = hist.axis.Regular(
+                        int(nbins),
+                        low,
+                        high,
+                        name="observable",
+                        label=observable_label,
+                    )
+                else:
+                    axis = hist.axis.Variable(
+                        observable_binning,
+                        name="observable",
+                        label=observable_label,
+                    )
+
+                histograms[chname][observable_name] = hist.Hist(
+                    axis,
+                    hist.axis.StrCategory([], name="process", growth=True),
+                    hist.axis.StrCategory([], name="variation", growth=True),
+                    storage=hist.storage.Weight(),
+                )
+        return histograms
+
+
+    def histogramming(
         self,
         object_copies: dict[str, ak.Array],
         events: ak.Array,
@@ -562,92 +628,6 @@ class ZprimeAnalysis:
                     weights
                 )  # return some dummy value to test auto-diff
 
-    def run_fit(
-        self, cabinetry_config: dict[str, Any]
-    ) -> tuple[Any, Any, Any, Any]:
-        """
-        Run the fit using cabinetry.
-
-        Parameters
-        ----------
-        cabinetry_config : dict
-            Configuration for cabinetry.
-        """
-
-        # what do we do with this
-        rebinning_router = get_cabinetry_rebinning_router(
-            cabinetry_config, rebinning=slice(110j, None, hist.rebin(2))
-        )
-        # build the templates
-        cabinetry.templates.build(cabinetry_config, router=rebinning_router)
-        # optional post-processing (e.g. smoothing, symmetrise)
-        cabinetry.templates.postprocess(cabinetry_config)
-        # build the workspace
-        ws = cabinetry.workspace.build(cabinetry_config)
-        # save the workspace
-        workspace_path = self.config.general.output_dir + "/statistics/"
-        os.makedirs(workspace_path, exist_ok=True)
-        workspace_path += "workspace.json"
-        cabinetry.workspace.save(ws, workspace_path)
-        # build the model and data
-        model, data = cabinetry.model_utils.model_and_data(ws)
-        # get pre-fit predictions
-        prefit_prediction = cabinetry.model_utils.prediction(model)
-        # perform the fit
-        results = cabinetry.fit.fit(
-            model,
-            data,
-        )  # perform the fit
-        postfit_prediction = cabinetry.model_utils.prediction(
-            model, fit_results=results
-        )
-
-        return data, results, prefit_prediction, postfit_prediction
-
-    def compute_ghost_observables(
-        self, obj_copies: dict[str, ak.Array], tracing: bool = False
-    ) -> dict[str, ak.Array]:
-        for ghost in self.config.ghost_observables:
-
-            # do not compute if this function is being traced by JAX
-            # and observable function is not compatible with JAX
-            if not ghost.works_with_jax and tracing:
-                continue
-            logger.info(f"Computing ghost observables {ghost.names}")
-            ghost_args = self._get_function_arguments(ghost["use"], obj_copies)
-            ghost_outputs = ghost["function"](*ghost_args)
-
-            if not isinstance(ghost_outputs, (list, tuple)):
-                ghost_outputs = [ghost_outputs]
-
-            names = (
-                ghost.names if isinstance(ghost.names, list) else [ghost.names]
-            )
-            colls = (
-                ghost.collections
-                if isinstance(ghost.collections, list)
-                else [ghost.collections] * len(names)
-            )
-            # update object_copies with ghost outputs
-            for out, name, coll in zip(ghost_outputs, names, colls):
-                if (
-                    isinstance(out, ak.Array)
-                    and len(ak.fields(out)) == 1
-                    and name in out.fields
-                ):
-                    out = out[name]
-                if coll in obj_copies:
-                    try:
-                        # add new field to existing awkward array
-                        obj_copies[coll][name] = out
-                    # happens if we are adding a field to an awkward
-                    # array with no fields (scalar fields)
-                    except ValueError as e:
-                        raise e
-                else:
-                    # create new awkward array with single field
-                    obj_copies[coll] = ak.Array({name: out})
-        return obj_copies
 
     def process(
         self, events: ak.Array, metadata: dict[str, Any], tracing: bool = False
@@ -720,7 +700,7 @@ class ZprimeAnalysis:
             for obj, var in obj_copies_corrected.items()
         }
         apply_selection_and_fill_grad = jax.value_and_grad(
-            self.apply_selection_and_fill, argnums=6, has_aux=False
+            self.histogramming, argnums=6, has_aux=False
         )
         val, grad = apply_selection_and_fill_grad(
             obj_copies_corrected,
@@ -740,7 +720,7 @@ class ZprimeAnalysis:
             obj: ak.to_backend(var, "cpu")
             for obj, var in obj_copies_corrected.items()
         }
-        self.apply_selection_and_fill(
+        self.histogramming(
             obj_copies_corrected,
             events,
             process,
@@ -770,7 +750,7 @@ class ZprimeAnalysis:
                     obj_copies, [syst], direction=direction
                 )
                 varname = f"{syst['name']}_{direction}"
-                self.apply_selection_and_fill(
+                self.histogramming(
                     obj_copies_corrected,
                     events,
                     process,
@@ -784,6 +764,47 @@ class ZprimeAnalysis:
                 )
 
 
+    def run_fit(
+        self, cabinetry_config: dict[str, Any]
+    ) -> tuple[Any, Any, Any, Any]:
+        """
+        Run the fit using cabinetry.
+
+        Parameters
+        ----------
+        cabinetry_config : dict
+            Configuration for cabinetry.
+        """
+
+        # what do we do with this
+        rebinning_router = get_cabinetry_rebinning_router(
+            cabinetry_config, rebinning=slice(110j, None, hist.rebin(2))
+        )
+        # build the templates
+        cabinetry.templates.build(cabinetry_config, router=rebinning_router)
+        # optional post-processing (e.g. smoothing, symmetrise)
+        cabinetry.templates.postprocess(cabinetry_config)
+        # build the workspace
+        ws = cabinetry.workspace.build(cabinetry_config)
+        # save the workspace
+        workspace_path = self.config.general.output_dir + "/statistics/"
+        os.makedirs(workspace_path, exist_ok=True)
+        workspace_path += "workspace.json"
+        cabinetry.workspace.save(ws, workspace_path)
+        # build the model and data
+        model, data = cabinetry.model_utils.model_and_data(ws)
+        # get pre-fit predictions
+        prefit_prediction = cabinetry.model_utils.prediction(model)
+        # perform the fit
+        results = cabinetry.fit.fit(
+            model,
+            data,
+        )  # perform the fit
+        postfit_prediction = cabinetry.model_utils.prediction(
+            model, fit_results=results
+        )
+
+        return data, results, prefit_prediction, postfit_prediction
 # -----------------------------
 # Main Driver
 # -----------------------------
@@ -799,7 +820,7 @@ def main():
     # ✅ You now have a fully validated config object
     logger.info(f"Luminosity: {config.general.lumi}")
 
-    analysis = ZprimeAnalysis(config)
+    analysis = NonDiffAnalysis(config)
     fileset = construct_fileset(
         n_files_max_per_sample=config.general.max_files
     )
